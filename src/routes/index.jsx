8102074321d1f54ed5e8f458d658578931928851
Andre Nascimento <andredossantosnascimento1510@gmail.com>
2021-06-08 15:06:52 -0300

    Feat/Fizemos a estrutura de pastas

---
+++ src/routes/index.jsx
@@ -0,0 +1,10 @@
+import { Route, Switch } from "react-router";
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/"></Route>
+    </Switch>
+  );
+};
+export default Routes;
