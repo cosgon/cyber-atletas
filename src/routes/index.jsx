e5b9c98cb565282803c52f9bf205c98f12c6f8b8
André dos Santos Nascimento <andredossantosnascimento1510@gmail.com>
2021-06-09 23:07:21 +0000

    Feature/group page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,13 @@
 import { Route, Switch } from "react-router";
+import GroupPage from "../pages/groupPage";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/"></Route>
+      <Route exact path="/groups">
+        <GroupPage />
+      </Route>
     </Switch>
   );
 };
