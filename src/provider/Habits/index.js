c3ceeea2c765c68c0b82338814f12e585a174114
Eduardo <egr.contato@gmail.com>
2021-06-12 17:15:09 -0400

    fix/ corrigindo a decodificação do token caso seja undefined

--- src/provider/Habits/index.js
+++ src/provider/Habits/index.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import jwt_decode from "jwt-decode";
@@ -8,15 +8,22 @@ import { toast } from "react-toastify";
 const HabitsContext = createContext();
 
 export const HabitsProvider = ({ children }) => {
+  const [habits, setHabits] = useState([]);
+  const [decoded, setDecoded] = useState({});
+  const { reset } = useForm();
+
   const api = axios.create({
     baseURL: "https://kabit-api.herokuapp.com/",
   });
 
   const token = JSON.parse(localStorage.getItem("token")) || "";
 
-  const decoded = jwt_decode(token);
-  const [habits, setHabits] = useState([]);
-  const { reset } = useForm();
+  useEffect(() => {
+    if (token) {
+      setDecoded(jwt_decode(token));
+      console.log(token);
+    }
+  }, []);
 
   const handleFormPost = (data) => {
     console.log("api", data);
