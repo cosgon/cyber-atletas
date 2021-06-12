4509f7edd5ee3af7966b841c25c6c1959f922b83
Andre Nascimento <andredossantosnascimento1510@gmail.com>
2021-06-12 13:54:49 -0300

    Fix: inclui o provider do register no index da pasta providers

--- src/provider/index.jsx
+++ src/provider/index.jsx
@@ -1,11 +1,13 @@
 import { HabitsProvider } from "./Habits";
 import { GroupsProvider } from "./groups";
-
+import { RegisterProvider } from "./Register";
 const Providers = ({ children }) => {
   return (
-    <GroupsProvider>
-      <HabitsProvider>{children}</HabitsProvider>;
-    </GroupsProvider>
+    <RegisterProvider>
+      <GroupsProvider>
+        <HabitsProvider>{children}</HabitsProvider>;
+      </GroupsProvider>
+    </RegisterProvider>
   );
 };
 export default Providers;
