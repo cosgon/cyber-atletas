f392d4e37ae0faf66ef84d08e38d4aaafecf716a
Andre Nascimento <andredossantosnascimento1510@gmail.com>
2021-06-14 10:36:38 -0300

    Fix: Resolvendo conflitos

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,8 +1,7 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Button, Avatar, Typography } from "@material-ui/core";
-
+import { useState } from "react";
 import CardDashboard from "../../components/CardDashboard";
-
 const useStyles = makeStyles({
   root: {
     backgroundColor: "#46344E",
@@ -57,13 +56,15 @@ const useStyles = makeStyles({
 const Dashboard = () => {
   const classes = useStyles();
 
+  const [display, setDisplay] = useState("habits");
+
+  const handleClick = (value) => {
+    setDisplay(value);
+    console.log(display);
+  };
+
   return (
-    <Grid
-      container
-      justify="space-evenly"
-      alignItems="center"
-      className={classes.root}
-    >
+    <Grid>
       <Grid
         container
         alignItems="center"
@@ -71,22 +72,57 @@ const Dashboard = () => {
         direction="column"
         className={classes.userBox}
       >
-        <Avatar className={classes.userImage} />
-        <Typography variant="h3" className={classes.userName}>
-          Nome do usuário
-        </Typography>
-        <Button variant="contained" className={classes.button}>
-          Hábitos
-        </Button>
-        <Button variant="contained" className={classes.button}>
-          Grupos
-        </Button>
-        <Button variant="contained" className={classes.button}>
-          Atividades
-        </Button>
-        <Button variant="contained" className={classes.button}>
-          Objetivos
-        </Button>
+        <Grid
+          container
+          alignItems="center"
+          justify="space-evenly"
+          direction="column"
+          className={classes.userBox}
+        >
+          <Avatar className={classes.userImage} />
+          <Typography variant="h3" className={classes.userName}>
+            Nome do usuário
+          </Typography>
+          <Button
+            variant="contained"
+            className={classes.button}
+            onClick={() => handleClick("habits")}
+          >
+            Hábitos
+          </Button>
+          <Button
+            variant="contained"
+            className={classes.button}
+            onClick={() => handleClick("groups")}
+          >
+            Grupos
+          </Button>
+          <Button
+            variant="contained"
+            className={classes.button}
+            onClick={() => handleClick("ativits")}
+          >
+            Atividades
+          </Button>
+          <Button
+            variant="contained"
+            className={classes.button}
+            onClick={() => handleClick("goals")}
+          >
+            Objetivos
+          </Button>
+        </Grid>
+        <Grid
+          container
+          alignItems="center"
+          justify="space-evenly"
+          className={classes.resumeBox}
+        >
+          {display === "habits" && <h2>Hábitos</h2>}
+          {display === "groups" && <h2>Grupos</h2>}
+          {display === "ativits" && <h2>Atividades</h2>}
+          {display === "goals" && <h2>Metas</h2>}
+        </Grid>
       </Grid>
       <Grid
         container
