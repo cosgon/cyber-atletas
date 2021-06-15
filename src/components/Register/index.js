88725596f7c9e965d6ad91adf4054d1aecddb383
Andre Nascimento <andredossantosnascimento1510@gmail.com>
2021-06-15 18:45:12 -0300

    Fix: Refatorando a feature register para implementar feedbacks visuais

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,81 +1,34 @@
-import { createMuiTheme, ThemeProvider, withStyles } from '@material-ui/core/styles';
-import { Button, TextField, Grid } from "@material-ui/core";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useRegister } from '../../provider/Register';
+import { useRegister } from '../../provider/Register/index.js';
+import useStyles, { CssTextField, theme } from './style.js';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Button, Grid } from "@material-ui/core";
+import { ToastContainer } from 'react-toastify';
 import SaveIcon from '@material-ui/icons/Save';
-import { useForm } from "react-hook-form";
-import { useHistory } from "react-router";
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
-import useStyles from './style.js';
-import * as yup from "yup";
 
-
-const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#FE6B8B',
-        },
-        secondary: {
-            main: '#faed26',
-        },
-    },
-});
-
-const CssTextField = withStyles({
-    root: {
-        '& .MuiOutlinedInput-root': {
-            '& fieldset': {
-                borderColor: '#faed26',
-            },
-            '&:hover fieldset': {
-                borderColor: 'yellow',
-            },
-        },
-    },
-})(TextField);
-
-const Register = () => {
-
+const Register = ({ schema }) => {
     const classes = useStyles();
-    const { registerUser } = useRegister();
-    const history = useHistory();
-
-    const schema = yup.object().shape({
-        username: yup
-            .string()
-            .required("Campo é obrigatório")
-            .max(18, "Limite máximo de 18 caracteres"),
-        email: yup
-            .string()
-            .email("Email inválido")
-            .required("Campo é obrigatório"),
-        password: yup
-            .string()
-            .min(8, "Mínimo de 8 dígitos")
-            .matches(
-                /^((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
-                "Senha deve conter ao menos uma letra maiúscula, uma minúscula, um número e um caracter especial!"
-            )
-            .required("Campo obrigatório!"),
-    });
-
 
     const {
         register,
         handleSubmit,
         reset,
         formState: { errors } }
-            = useForm(
-        { resolver: yupResolver(schema), });
+        = useForm(
+            { resolver: yupResolver(schema), });
 
-    const handleMyForm = async (data) => {
-        await registerUser(data);
+    const { registerUser } = useRegister();
+
+    const handleMyForm = (data) => {
+        registerUser(data);
         reset();
-        history.push("/PageLogin");
     };
 
     return (
         <ThemeProvider theme={theme}>
+            <ToastContainer />
             <form
                 className={classes.root}
                 onSubmit={handleSubmit(handleMyForm)}
@@ -115,6 +68,7 @@ const Register = () => {
                             margin="normal"
                             variant="outlined"
                             size="small"
+                            type="password"
                             color="primary"
                             {...register("password")}
                         />
@@ -136,7 +90,6 @@ const Register = () => {
                     Home
                 </Link>
             </form >
-
         </ThemeProvider>
     );
 }
