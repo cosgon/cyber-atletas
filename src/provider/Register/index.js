8932c85c4e2290ca97dd6a21ba33efeb84892c45
Andre Nascimento <andredossantosnascimento1510@gmail.com>
2021-06-15 17:11:08 -0300

    Fix: Refatorado codigo do register!

--- src/provider/Register/index.js
+++ src/provider/Register/index.js
@@ -1,19 +1,21 @@
-import { createContext,useContext} from 'react';
+import { createContext, useContext } from 'react';
+import { toast } from 'react-toastify';
 import axios from "axios";
 
 export const RegisterContext = createContext();
 
 export const RegisterProvider = ({ children }) => {
 
-   const registerUser = async(data) =>{
-       await axios 
-        .post("https://kabit-api.herokuapp.com/users/", data)
-        .catch((e) => console.log(e));
+   const registerUser = (data) => {
+      axios
+         .post("https://kabit-api.herokuapp.com/users/", data)
+         .then(() => toast.success("Cadastrado com sucesso!"))
+         .catch((e) => toast.error(`Erro ao cadastrar: ${e}!`))
    }
 
-        return (
-           <RegisterContext.Provider value={{registerUser}}> {children} </RegisterContext.Provider>
-	); 
+   return (
+      <RegisterContext.Provider value={{ registerUser }}> {children} </RegisterContext.Provider>
+   );
 };
 
 export const useRegister = () => useContext(RegisterContext);
