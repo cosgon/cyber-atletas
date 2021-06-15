d5c60182cab7d08fc4881ff51d5610959e4ebbbd
Andre Nascimento <andredossantosnascimento1510@gmail.com>
2021-06-15 17:03:02 -0300

    Fix:Refatorado contexto de login para utillizar estados para salvar o id do usuario e o token

--- src/provider/Login/index.js
+++ src/provider/Login/index.js
@@ -1,34 +1,36 @@
-import { createContext, useContext } from "react";
-import axios from "axios";
-import jwt_decode from "jwt-decode";
-import { toast } from "react-toastify";
+import { createContext, useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
 
 export const LoginContext = createContext();
 
 const api = axios.create({
   baseURL: "https://kabit-api.herokuapp.com",
 });
+
 export const LoginProvider = ({ children }) => {
+  const [userId, setUserId] = useState(0)
+  const [token, setToken] = useState("")
   const history = useHistory();
 
   const loginUser = (data) => {
     api
       .post("/sessions/", data)
-      .then((response) => {
-        const { user_id } = jwt_decode(response.data.access);
-        localStorage.clear();
-        localStorage.setItem("token", JSON.stringify(response.data.access));
-        localStorage.setItem("user_id", JSON.stringify(user_id));
+      .then(({ data }) => {
+        const { user_id } = jwt_decode(data.access);
+        setToken(data.access);
+        setUserId(user_id);
         history.push("/dashboard");
       })
-      .catch((e) => {
+      .catch(() => {
         toast.error("Verifique seu email ou senha!")
       });
   }
 
   return (
-    <LoginContext.Provider value={{ loginUser }}> {children} </LoginContext.Provider>
+    <LoginContext.Provider value={{ loginUser, userId, token }}> {children} </LoginContext.Provider>
   );
 };
 
