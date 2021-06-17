8ae4d2a41c7a389406296763fc005efa7419c64d
Andre Nascimento <andredossantosnascimento1510@gmail.com>
2021-06-16 22:25:38 -0300

    Feat: Estilizado a showActivities e o botao de adicionar atividades

--- src/components/ShowActivities/style.js
+++ src/components/ShowActivities/style.js
@@ -1,30 +1,59 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
-  card: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    color: "#ffffff",
-    borderRadius: "2rem",
-    textAlign: "center",
-    marginTop: "20px",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexWrap: "wrap",
-  },
-  loading: {
-    color: "#faed26",
-    margin: "50vh auto",
-  },
-  buttonRemove: {
-    borderRadius: "100%",
-    "&:hover": {
-      transition: "0.6s",
-    },
-    color: "#FFFFFF",
-    margin: "0 20px",
+  container: {
+    backgroundColor: "#00000000",
 
   },
-});
+  containerActivity: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "5px 0",
+    borderRadius: "2.3rem",
+    color: "#FFFFFF",
+  },
+  title: {
+    width: "70%",
+    textAlign: "center",
+    "&& h4": {
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+    }
+  }
+}
+  /*   
+  {
+    card: {
+      background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+      color: "#ffffff",
+      borderRadius: "2rem",
+      textAlign: "center",
+      marginTop: "20px",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      flexWrap: "wrap",
+    },
+    loading: {
+      color: "#faed26",
+      margin: "50vh auto",
+    },
+    buttonRemove: {
+      borderRadius: "100%",
+      "&:hover": {
+        transition: "0.6s",
+      },
+      color: "#FFFFFF",
+      margin: "0 20px",
+  
+    },
+  }
+  */
+
+);
 
 export default useStyles;
