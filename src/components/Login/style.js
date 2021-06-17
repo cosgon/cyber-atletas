5750d48b3e11d3b2fb6e82fd8060aa780911d1f8
Charles Pinheiro <charles_fagundes@hotmail.com>
2021-06-17 12:25:09 -0300

    fix: padronização no estilo da home, login e register

--- src/components/Login/style.js
+++ src/components/Login/style.js
@@ -5,7 +5,6 @@ const useStyles = makeStyles((theme) => ({
 
    root: {
       backgroundColor: '#46344E',
-      flexGrow: '1',
       minHeight: '100vh',
       height: '100%',
       width: '100%',
@@ -20,35 +19,33 @@ const useStyles = makeStyles((theme) => ({
       width: '30%',
    },
 
-  button: {
-   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    borderRadius: 3,
-    border: 0,
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    margin: "10% auto",
+   button: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      borderRadius: '5px',
+      color: '#FFF',
+      height: '3rem',
+      padding: '0 2rem',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      margin: "10% auto",
    },
 
    link: {
       textDecoration: "none",
       margin: "2px 0", 
-      color: "#D9D9D9", 
-    },
+      color: "#D9D9D9",
+      '&:hover': {
+         filter: 'brightness(2)'
+      }
+   },
 
-    textField: {
-       borderColor: "#D9D9D9",
-    },
-
-    box : {
+   box : {
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
       maxWidth: '415px',
-      color: 'red',
-    }
+      color: '#EF5350',
+   }
 }));
 
 export const theme = createMuiTheme({
@@ -79,4 +76,4 @@ export const CssTextField = withStyles({
    },
 })(TextField);
 
-export default useStyles;           
+export default useStyles;
--- src/components/Register/style.js
+++ src/components/Register/style.js
@@ -6,8 +6,7 @@ import { TextField } from "@material-ui/core";
 const useStyles = makeStyles((theme) => ({
 
    root: {
-      backgroundColor: '#46344e',
-      flexGrow: '1',
+      backgroundColor: '#46344E',
       minHeight: '100vh',
       height: '100%',
       width: '100%',
@@ -24,11 +23,11 @@ const useStyles = makeStyles((theme) => ({
 
    button: {
       background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      borderRadius: 3,
+      borderRadius: '5px',
       border: 0,
       color: 'white',
-      height: 48,
-      padding: '0 30px',
+      height: '3rem',
+      padding: '0 2rem',
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
       margin: "10% auto",
    },
@@ -37,7 +36,10 @@ const useStyles = makeStyles((theme) => ({
 
       textDecoration: "none",
       margin: "2px 0",
-      color: "#d9d9d9",
+      color: "#D9D9D9",
+      '&:hover': {
+         filter: 'brightness(2)'
+      }
    },
 
    box: {
@@ -45,8 +47,8 @@ const useStyles = makeStyles((theme) => ({
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
-      maxWidth: '415px',
-      color: 'red',
+      maxWidth: '26rem',
+      color: '#EF5350',
    },
 
 }));
@@ -57,7 +59,7 @@ export const theme = createMuiTheme({
          main: '#FE6B8B',
       },
       secondary: {
-         main: '#faed26',
+         main: '#FAED26',
       },
    },
 });
@@ -65,7 +67,7 @@ export const theme = createMuiTheme({
 export const CssTextField = withStyles({
    root: {
       '&& fieldset': {
-         borderColor: '#faed26',
+         borderColor: '#FAED26',
       },
       '&&:hover fieldset': {
          borderColor: '#FAED26',
