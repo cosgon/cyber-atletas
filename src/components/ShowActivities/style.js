d5d6219bbabd02ebef314d4ecae29c02c6728e42
Andre Nascimento <andredossantosnascimento1510@gmail.com>
2021-06-16 21:25:12 -0300

    Feat: Estilizado pagina do grupo selecionado!

--- src/components/ShowActivities/style.js
+++ src/components/ShowActivities/style.js
@@ -4,13 +4,13 @@ const useStyles = makeStyles({
   card: {
     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
     color: "#ffffff",
-    width: "34rem",
     borderRadius: "2rem",
     textAlign: "center",
     marginTop: "20px",
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
+    flexWrap: "wrap",
   },
   loading: {
     color: "#faed26",
