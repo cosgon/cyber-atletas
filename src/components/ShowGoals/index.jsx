bb9ff246f322ce2cdd76612a16c22d88dde00086
Eduardo Godoi <egr.contato@gmail.com>
2021-06-17 00:32:55 +0000

    Feature/goals

--- src/components/ShowGoals/index.jsx
+++ src/components/ShowGoals/index.jsx
@@ -1,15 +1,35 @@
 import useStyles from "./style";
-import { Card, CardContent, CircularProgress, Fab } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  CircularProgress,
+  Fab,
+  Button,
+  LinearProgress,
+} from "@material-ui/core";
 import { useGroups } from "../../provider/Groups";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const ShowGoals = ({ id }) => {
   const classes = useStyles();
 
-  const { goals, loading, selected, getGoals, deleteGoal } = useGroups();
+  const {
+    getOneGoal,
+    counter,
+    updateGoals,
+    setCounter,
+    goals,
+    loading,
+    selected,
+    getGoals,
+    deleteGoal,
+  } = useGroups();
+
+  console.log(counter);
 
   useEffect(() => {
     getGoals(selected);
+    getOneGoal(goals.id);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return loading || false ? (
@@ -23,7 +43,37 @@ const ShowGoals = ({ id }) => {
           <CardContent>
             <h3>Titulo: {goals.title}</h3>
             <p> Dificuldade: {goals.difficulty}</p>
-            <p>Pontos: {goals.how_much_achieved}</p>
+            <div className={classes.progress}>
+              <Button
+                className={classes.increment}
+                variant="contained"
+                onClick={() => {
+                  setCounter(counter + 10);
+                  updateGoals(goals.id);
+                }}
+              >
+                {" "}
+                +
+              </Button>
+              <div className={classes.linearBar}>
+                <LinearProgress
+                  variant="determinate"
+                  value={goals.how_much_achieved}
+                />
+              </div>
+
+              <Button
+                className={classes.increment}
+                variant="contained"
+                onClick={() => {
+                  setCounter(counter - 10);
+                  updateGoals(goals.id);
+                }}
+              >
+                {" "}
+                -
+              </Button>
+            </div>
           </CardContent>
           <CardContent>
             <Fab
